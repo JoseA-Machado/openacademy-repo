47adeaf75bdbb2957ade8d8884d6bfb15b524637
Jose Antonio Machado Madero <josean.machado@icloud.com>
2020-09-30 11:59:11 -0600

    [REF] computed fields and default

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+import time
 
+def get_uid(self, *a):
+    return self.env.uid
 
 class Course(models.Model):
     _name = 'openacademy.course'
@@ -10,7 +13,9 @@ class Course(models.Model):
     name = fields.Char(string='Title', required=True)
     description = fields.Text()
     responsible_id = fields.Many2one('res.users',
-        ondelete='set null', string="Responsible", index=True)
+        ondelete='set null', string="Responsible", index=True, 
+        #default=lambda self, *a: self.env.uid)
+        default=get_uid)
     session_ids = fields.One2many(
         'openacademy.session', 'course_id')
 
@@ -19,10 +24,23 @@ class Session(models.Model):
     _description = 'Model to store seats'
 
     name = fields.Char(required=True)
-    start_date = fields.Date()
+    start_date = fields.Date(default=fields.Date.today)
+    datetime_test = fields.Datetime(default=fields.Datetime.now)
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
     instructor_id = fields.Many2one('res.partner', string="Instructor", domain=['|', ('instructor','=',True), ('category_id.name', 'ilike', 'Teacher')])
     course_id = fields.Many2one('openacademy.course',
         ondelete='cascade', string="Course", required=True)
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
+    taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
+    active = fields.Boolean(default=True)
+
+    @api.depends('seats', 'attendee_ids')
+    def _taken_seats(self):
+        #for r in self.filtered(lambda r: r.seats):
+        #    r.taken_seats = 100.0 * len(r.attendee_ids) / r.seats
+        for r in self:
+            if not r.seats:
+                r.taken_seats = 0.0
+            else:
+                r.taken_seats = 100.0 * len(r.attendee_ids) / r.seats
